49957c3a76ff035c2379f88d8bfaed5fd1c6452b
gammafn <GammaFunction@vivaldi.net>
2020-01-06 15:58:03 -0600

    Switch to underscores for consistancy

--- p10k-dir-glob.zsh
+++ p10k-dir-glob.zsh
@@ -4,7 +4,7 @@
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 fpath+=(${0:h})
-autoload -Uz .prompt_dir-glob::format-dir
+autoload -Uz .prompt_dir_glob::format_dir
 
 declare -ga prompt_dir_glob__globs
 declare -gA prompt_dir_glob__{prefix,suffix,truncate}
@@ -16,7 +16,7 @@ declare -gA __prompt_dir_glob__cache
 [[ -r $PROMPT_DIR_GLOB__CACHE_FILE ]] &&
 	. $PROMPT_DIR_GLOB__CACHE_FILE
 
-function .prompt_dir-glob::dir-gw(){
+function .prompt_dir_glob::dir_gw() {
 	zmodload zsh/parameter
 	for g ($usergroups); do
 		local f=("${REPLY}"(Eg$g))
@@ -25,19 +25,19 @@ function .prompt_dir-glob::dir-gw(){
 	return 1
 }
 
-function prompt_dir-glob::flush-cache(){
+function prompt_dir_glob::flush_cache() {
 	typeset -p __prompt_dir_glob__cache > $PROMPT_DIR_GLOB__CACHE_FILE
 	zcompile $PROMPT_DIR_GLOB__CACHE_FILE
 }
 
 # Clear cache, or clear cache under given directory
 # relative paths are not supported
-function prompt_dir-glob::clear-cache(){
+function prompt_dir_glob::clear_cache() {
 	if [[ -n $1 ]]; then
 		for dir in $__prompt_dir_glob__cache[(I)(${1%/}|${1%/}/*)]; do
 			unset "__prompt_dir_glob__cache[$dir]"
 		done
-		prompt_dir-glob::flush-cache
+		prompt_dir_glob::flush_cache
 	else
 		__prompt_dir_glob__cache=( )
 		: > $PROMPT_DIR_GLOB__CACHE_FILE
@@ -45,7 +45,7 @@ function prompt_dir-glob::clear-cache(){
 }
 
 # A front-end to prompt_dir_glob* arrays
-function prompt_dir-glob::add-glob() {
+function prompt_dir_glob::add_glob() {
 	zmodload zsh/zutil
 	local -a glob prefix suffix truncate 
 	local f g
@@ -58,7 +58,7 @@ function prompt_dir-glob::add-glob() {
 	done
 }
 
-function prompt_dir-glob(){
+function prompt_dir_glob() {
 	setopt -L nullglob
 
 	local head dir show actual actual_init show_init remainder
@@ -73,7 +73,7 @@ function prompt_dir-glob(){
 	for dir ("${actual_init%/}" ${(s:/:)remainder}); do
 		# handle root case
 		if [[ -z $dir ]]; then
-			if ! zstyle -t :dir-glob seperate-sections; then
+			if ! zstyle -t :dir_glob seperate-sections; then
 				dir_parts+=("$PROMPT_DIR_GLOB__SEPARATOR")
 				head=/
 				continue
@@ -85,7 +85,7 @@ function prompt_dir-glob(){
 			# use cache
 			dir_parts+=($__prompt_dir_glob__cache[$head$dir])
 		else
-			.prompt_dir-glob::format-dir
+			.prompt_dir_glob::format_dir
 		fi
 		unset show_init
 		dir_parts+=("$PROMPT_DIR_GLOB__SEPARATOR")
@@ -93,5 +93,5 @@ function prompt_dir-glob(){
 	done
 
 	# don't append final separator
-	p10k segment -t ${(j::)dir_parts[1,-2]}
+	p10k segment -t ${(j::)dir_parts[1,_2]}
 }
