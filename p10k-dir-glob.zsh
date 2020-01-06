33c2b7e35c14ea875a4a43dad44c7c6f0650a0e9
gammafn <GammaFunction@vivaldi.net>
2020-01-05 20:55:47 -0600

    Bug fix: prefix/suffix/truncate is always defined

--- p10k-dir-glob.zsh
+++ p10k-dir-glob.zsh
@@ -52,9 +52,9 @@ function prompt_dir-glob::add-glob() {
 	zparseopts - g:=glob -pre:=prefix -suf:=suffix t:=truncate
 	for f g in "${(@)glob}"; do
 		prompt_dir_glob__globs+=("$g")
-		[[ -v prefix ]] && prompt_dir_glob__prefix[$g]=$prefix[-1]
-		[[ -v suffix ]] && prompt_dir_glob__suffix[$g]=$suffix[-1]
-		[[ -v truncate ]] && prompt_dir_glob__truncate[$g]=$truncate[-1]
+		(( $#prefix   )) && prompt_dir_glob__prefix[$g]=$prefix[-1]
+		(( $#suffix   )) && prompt_dir_glob__suffix[$g]=$suffix[-1]
+		(( $#truncate )) && prompt_dir_glob__truncate[$g]=$truncate[-1]
 	done
 }
 
