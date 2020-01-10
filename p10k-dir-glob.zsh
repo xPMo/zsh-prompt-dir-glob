3e963fc33e90662282af40ff854ca3dcac3f97cf
gammafn <GammaFunction@vivaldi.net>
2020-01-10 10:33:19 -0600

    Add caching truncates

--- p10k-dir-glob.zsh
+++ p10k-dir-glob.zsh
@@ -8,7 +8,7 @@ autoload -Uz prompt_dir_glob{,::add_glob}
 
 declare -ga prompt_dir_glob__globs
 declare -gA prompt_dir_glob__{prefix,suffix,truncate}
-declare -gA __prompt_dir_glob__cache
+declare -gA __prompt_dir_glob__{truncate_,}cache
 : ${PROMPT_DIR_GLOB__CACHE_FILE:="${XDG_CACHE_HOME:-$HOME/.cache}/p10k_dir_glob.cache"}
 : ${PROMPT_DIR_GLOB__SEPARATOR:=${POWERLEVEL9K_DIR_SEPARATOR:-'/'}}
 
@@ -24,7 +24,7 @@ function .prompt_dir_glob::is_dir_gw() {
 }
 
 function prompt_dir_glob::flush_cache() {
-	typeset -p __prompt_dir_glob__cache > $PROMPT_DIR_GLOB__CACHE_FILE
+	typeset -p __prompt_dir_glob__{truncate_,}cache > $PROMPT_DIR_GLOB__CACHE_FILE
 	zcompile $PROMPT_DIR_GLOB__CACHE_FILE
 }
 
