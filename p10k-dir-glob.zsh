0e5d6484dc8fb9a49386c91afbd853a4a7ac74c8
gammafn <GammaFunction@vivaldi.net>
2020-01-10 10:15:43 -0600

    Re-merge code into a single function
    Now prompt_dir_glob itself is autoloaded

--- p10k-dir-glob.zsh
+++ p10k-dir-glob.zsh
@@ -4,7 +4,7 @@
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 fpath+=(${0:h})
-autoload -Uz .prompt_dir_glob::format_dir prompt_dir_glob::add_glob
+autoload -Uz prompt_dir_glob{,::add_glob}
 
 declare -ga prompt_dir_glob__globs
 declare -gA prompt_dir_glob__{prefix,suffix,truncate}
@@ -37,37 +37,4 @@ function prompt_dir_glob::clear_cache() {
 		__prompt_dir_glob__cache=( )
 		: > $PROMPT_DIR_GLOB__CACHE_FILE
 	fi
-}
-
-function prompt_dir_glob() {
-	setopt -L nullglob
-
-	local head dir show actual actual_init show_init remainder
-	show=${(%):-'%~'}
-	actual=${(%):-'%/'}
-	actual_init=${actual%${show#*/}}
-	show_init=${show%%/*}
-	remainder=${actual#$actual_init}
-
-	local glob
-	local -a dir_parts
-	for dir ("${actual_init%/}" ${(s:/:)remainder}); do
-		# handle root case
-		if [[ -z $dir ]]; then
-			if zstyle -t :dir_glob include-root; then
-				dir=/
-			else
-				head=/
-			fi
-		fi
-		.prompt_dir_glob::format_dir
-
-		unset show_init
-		# reset foreground style
-		dir_parts+=("$PROMPT_DIR_GLOB__SEPARATOR%b%f%u")
-		head+=${dir}/
-	done
-
-	# don't append final separator
-	p10k segment -t ${(j::)dir_parts[1,-2]}
 }
