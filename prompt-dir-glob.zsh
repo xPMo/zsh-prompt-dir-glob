43814c3d49d6288b3499166cc5645460cb1a438f
gammafn <GammaFunction@vivaldi.net>
2020-01-21 12:31:48 -0600

    Remove p10k from cache file

--- prompt-dir-glob.zsh
+++ prompt-dir-glob.zsh
@@ -12,7 +12,7 @@ autoload -Uz prompt_dir_glob::{build,add_glob,clear_cache,is_dir_gw}
 declare -ga prompt_dir_glob__globs
 declare -gA prompt_dir_glob__{prefix,suffix,truncate}
 declare -gA __prompt_dir_glob__{truncate_,}cache
-: ${PROMPT_DIR_GLOB__CACHE_FILE:="${XDG_CACHE_HOME:-$HOME/.cache}/p10k_dir_glob.cache"}
+: ${PROMPT_DIR_GLOB__CACHE_FILE:="${XDG_CACHE_HOME:-$HOME/.cache}/prompt_dir_glob.cache.zsh"}
 : ${PROMPT_DIR_GLOB__SEPARATOR:=${POWERLEVEL9K_DIR_SEPARATOR:-'/'}}
 
 [[ -r $PROMPT_DIR_GLOB__CACHE_FILE ]] && . $PROMPT_DIR_GLOB__CACHE_FILE
